3c8faa5def4dcdc44fee7c8ae5edabf675082f0f
i-niranjan <niranjanchaudhari2004@gmail.com>
2025-04-02 00:06:27 +0530

    Added BAsic Footer

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
@@ -5,6 +6,7 @@ export default function Dashboard() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
